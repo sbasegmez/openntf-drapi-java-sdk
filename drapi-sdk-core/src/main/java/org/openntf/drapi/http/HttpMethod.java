0f8e4b69fa6b91b50e6793296ca66165efad75f3
Serdar Basegmez <2338355+sbasegmez@users.noreply.github.com>
2026-08-27 13:19:10 +0100

    feat: Convenience method for the HttpMethod

--- drapi-sdk-core/src/main/java/org/openntf/drapi/http/HttpMethod.java
+++ drapi-sdk-core/src/main/java/org/openntf/drapi/http/HttpMethod.java
@@ -5,5 +5,15 @@ public enum HttpMethod {
     GET,
     PATCH,
     POST,
-    PUT
+    PUT;
+
+    public static HttpMethod of(String method) {
+        for (HttpMethod httpMethod : values()) {
+            if (httpMethod.name().equalsIgnoreCase(method)) {
+                return httpMethod;
+            }
+        }
+        throw new IllegalArgumentException("Unknown HTTP method: " + method);
+    }
+
 }
