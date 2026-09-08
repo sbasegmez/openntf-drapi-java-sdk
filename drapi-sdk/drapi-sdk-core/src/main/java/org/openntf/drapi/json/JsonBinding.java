e1c9f1796166d30c08c497bcc18eb1aa4d296196
Serdar Basegmez <2338355+sbasegmez@users.noreply.github.com>
2026-09-08 13:03:14 +0100

    docs: missing javadocs added to JsonBinding

--- drapi-sdk/drapi-sdk-core/src/main/java/org/openntf/drapi/json/JsonBinding.java
+++ drapi-sdk/drapi-sdk-core/src/main/java/org/openntf/drapi/json/JsonBinding.java
@@ -33,19 +33,75 @@ import org.openntf.drapi.util.ServiceRegistry;
  */
 public interface JsonBinding {
 
+    /**
+     * Returns the name of the JSON implementation. This name can be used to identify the specific JSON library or framework being
+     * used.
+     *
+     * @return the name of the JSON implementation
+     */
     String name();
 
+    /**
+     * Deserializes JSON data from the provided InputStream into a Map<String, Object>. The resulting map represents the JSON structure,
+     * where keys are JSON field names and values are the corresponding values.
+     *
+     * @param jsonStream the InputStream containing the JSON data
+     * @return a Map representing the deserialized JSON structure
+     */
     Map<String, Object> fromJson(InputStream jsonStream);
+
+    /**
+     * Deserializes JSON data from the provided InputStream into an instance of the specified valueType. The resulting object is
+     * populated with the data from the JSON structure.
+     * <p>
+     * This is useful to serialise simple Json objects into a Java object of a specific type, allowing for type-safe access to the data.
+     * Since we cannot support library-specific annotations in this project, more advanced serialisation/deserialisation features (like
+     * custom field names, ignoring fields, etc.) are not supported. For more advanced use cases, consider using the underlying JSON
+     * library directly.
+     *
+     * @param jsonStream the InputStream containing the JSON data
+     * @param valueType  the class of the type to deserialize into
+     * @param <T>        the type of the resulting object
+     * @return an instance of the specified type populated with the JSON data
+     */
     <T> T fromJson(InputStream jsonStream, Class<T> valueType);
 
+    /**
+     * Serializes the provided objectValue into JSON format and writes it to the provided OutputStream. The objectValue can be any Java
+     * object that can be represented in JSON format.
+     *
+     * @param objectValue  the object to be serialized into JSON
+     * @param outputStream the OutputStream to write the JSON data to
+     */
     void toJson(Object objectValue, OutputStream outputStream);
 
+    /**
+     * Deserializes JSON data from the provided jsonString into a Map<String, Object>. The resulting map represents the JSON structure,
+     * where keys are JSON field names and values are the corresponding values.
+     *
+     * @param jsonString the JSON string to be deserialized
+     * @return a Map representing the deserialized JSON structure
+     */
     default Map<String, Object> fromJson(String jsonString) {
         return fromJson(
             new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8))
         );
     }
 
+    /**
+     * Deserializes JSON data from the provided jsonString into an instance of the specified valueType. The resulting object is
+     * populated with the data from the JSON structure.
+     * <p>
+     * This is useful to serialise simple Json objects into a Java object of a specific type, allowing for type-safe access to the data.
+     * Since we cannot support library-specific annotations in this project, more advanced serialisation/deserialisation features (like
+     * custom field names, ignoring fields, etc.) are not supported. For more advanced use cases, consider using the underlying JSON
+     * library directly.
+     *
+     * @param jsonString the JSON string to be deserialized
+     * @param valueType  the class of the type to deserialize into
+     * @param <T>        the type of the resulting object
+     * @return an instance of the specified type populated with the JSON data
+     */
     default <T> T fromJson(String jsonString, Class<T> valueType) {
         return fromJson(
             new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8)),
@@ -53,17 +109,34 @@ public interface JsonBinding {
         );
     }
 
+    /**
+     * Serializes the provided objectValue into JSON format and returns it as a String. The objectValue can be any Java object that can
+     * be represented in JSON format.
+     *
+     * @param objectValue the object to be serialized into JSON
+     * @return a String containing the serialized JSON representation of the object
+     */
     default String toJson(Object objectValue) {
         var outputStream = new java.io.ByteArrayOutputStream();
         toJson(objectValue, outputStream);
         return outputStream.toString(StandardCharsets.UTF_8);
     }
 
+    /**
+     * Returns the default instance of JsonBinding. This instance is lazily loaded and can be overridden for testing purposes.
+     *
+     * @return the default instance of JsonBinding
+     */
     static JsonBinding get() {
         return JsonBindingHolder.getInstance();
     }
 
+    /**
+     * Holder class for the default instance of JsonBinding. This class is responsible for lazy-loading the singleton instance and
+     * providing methods to override or reset it for testing purposes.
+     */
     final class JsonBindingHolder {
+
         // Lazy-loaded singleton instance of JsonBinding
         private static volatile JsonBinding defaultInstance;
 
@@ -94,7 +167,8 @@ public interface JsonBinding {
             overriddenInstance = null;
         }
 
-        private JsonBindingHolder() {}
+        private JsonBindingHolder() {
+        }
     }
 
 }
